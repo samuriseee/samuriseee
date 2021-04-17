0e041d8b4aff99ed362f83ed7d59b8d6074390ac
samuriseee <gonhungho123@gmail.com>
2021-04-18 01:54:03 +0700

    update bai8

--- buoi8lan3/buoi8lan3.js
+++ buoi8lan3/buoi8lan3.js
@@ -40,6 +40,11 @@ function readURL(input) {
 
 function addHTMLTableRow(){
     if (checkInput()) {
+        const item = {
+        _name : name,
+        _category   : category,
+        _img : reader.result
+        };
         window.rowCount++;
         let table = document.getElementById("table"),
             newRow=table.insertRow(table.length),
@@ -85,8 +90,8 @@ function selectedRowToInput()
     }
     selectedRowToInput();
 
-function editHtmlTbleSelectedRow() {   
-    if (checkInput()) {
+function editHtmlTbleSelectedRow()
+    {
         let name = document.getElementById("itemName").value,
             category = document.getElementById("Category").value;
             
@@ -97,20 +102,18 @@ function editHtmlTbleSelectedRow() {
             table.rows[rIndex].cells[3].innerHTML = `  
                 <img id="idimg3" src="" alt=" "> 
             `;
-           
+
 
 
             let img = document.getElementById("idimg3");
             img.setAttribute("src", "data:image/png;base64," + localStorage.getItem("lastImg"));
-    }
+            
             selectedRowToInput();
       
     }
 function removeSelectedRow() {
-    
             table.deleteRow(rIndex);
             document.getElementById("itemName").value="";
             document.getElementById("Category").value="";
             document.getElementById("idimg").value="";
-    }
 }
