98f26466c1c4f62916e7df382c65145950e5a892
samuriseee <gonhungho123@gmail.com>
2021-05-08 15:57:27 +0700

    begin coding sass

--- .vscode/CV sass/sass.js
+++ .vscode/CV sass/sass.js
@@ -1,4 +1,4 @@
-let stars = document.getElementById('stars');
+        let stars = document.getElementById('stars');
         let moon = document.getElementById('moon');
         let mountain_beind = document.getElementById('mountain_behind');
         let text = document.getElementById('text');
